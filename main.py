e3ef05023ea17d9f631ce110bd66ee4283c2299f
hagaitrg <disahagait@gmail.com>
2021-11-06 04:47:17 +0700

    adding reaction and embed messages

--- main.py
+++ main.py
@@ -17,8 +17,26 @@ async def on_ready():
 
 
 @bot.command(pass_context=True)
-async def hello(ctx):
-    await ctx.send("Hallo jaga kesehatan dan jangan lupa minum air putih!")
+async def hello(ctx, name: str):
+    await ctx.send(f"Hallo {name} jaga kesehatan dan jangan lupa minum air putih!")
+
+
+@bot.command(pass_context=True)
+async def bye(ctx):
+    embed = discord.Embed(
+        title="Hello I am leaving now"
+    )
+
+    msg = await ctx.send(embed=embed)
+    await msg.add_reaction('✅')
+
+
+@bot.command(pass_context=True)
+async def clear(ctx, amount: str):
+    if amount == 'all':
+        await ctx.channel.purge()
+    else:
+        await ctx.channel.purge(limit=(int(amount)+1))
 
 
 bot.run(API_TOKEN, bot=True, reconnect=True)
